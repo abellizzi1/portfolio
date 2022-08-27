73d7a07b20a056d90daadbae65d00b99a2f2b7f7
abellizzi1 <abelliz734@gmail.com>
2022-08-27 16:55:55 -0500

    switched from react

---
+++ app.js
@@ -0,0 +1,60 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 2500,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+// const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+// let scene2 = new ScrollMagic.Scene({
+//   duration: 3000,
+//   triggerElement: intro,
+//   triggerHook: 0
+// })
+//   .setTween(textAnim)
+//   .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 33.3);
+
+
+const starBackground = document.querySelector('#starsBackground');
+const scrollAmount = 0;
+window.addEventListener('scroll', (event) => {
+  const { top } = starBackground.getBoundingClientRect();
+  const starsInView = top - window.innerHeight < scrollAmount;
+  if (starsInView) {
+    intro.style.opacity=0;
+  }
+  else {
+    intro.style.opacity=1;
+  }
+});
